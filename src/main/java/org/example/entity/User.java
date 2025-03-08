5ad355f65163bd7450b5e84ee5ebb4e5f134b497
vitor <vitorsalves0812@gmail.com>
2025-03-08 19:30:47 -0300

    first commit

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,25 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate birthday;
+    private double height;
+    private double weight;
+    private boolean active;
+
+}
